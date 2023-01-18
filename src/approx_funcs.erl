3e46d83c59fc7ba2eb56de5b26aaa700a397a84e
Rain <hramil0120@gmail.com>
2023-01-18 23:12:39 +0300

    Apply generators in separate processes

--- src/approx_funcs.erl
+++ src/approx_funcs.erl
@@ -5,7 +5,7 @@
 
 -include("approx_funcs_methods.hrl").
 
--record(approx_params, {step = 0.2, approx_funcs = [l, e, p], max_points = 10}).
+-record(approx_params, {step = 0.2, approx_funcs = [l, e, p], points_window = 3}).
 
 %%====================================================================
 %% API functions
@@ -14,19 +14,12 @@
 %% escript Entry point
 main(Args) ->
     Params = parse_parameters(#approx_params{}, Args),
-    process_points([], Params),
+    Subscribers = spawn_generators(Params),
+    process_points(Params, Subscribers),
     erlang:halt(0).
 
 parse_parameters(Result, [Arg, NextArg | Tail]) ->
     case Arg of
-        "-mp" ->
-            case string:to_integer(NextArg) of
-                {error, _} ->
-                    io:fwrite("Wrong -mp argument value: ~p ~n", [NextArg]),
-                    exit(1);
-                {Int, _} ->
-                    parse_parameters(Result#approx_params{max_points = Int}, Tail)
-            end;
         "-step" ->
             case string:to_float(NextArg) of
                 {error, _} ->
@@ -34,6 +27,14 @@ parse_parameters(Result, [Arg, NextArg | Tail]) ->
                     exit(1);
                 {Float, _} ->
                     parse_parameters(Result#approx_params{step = Float}, Tail)
+            end;
+        "-pw" ->
+            case string:to_float(NextArg) of
+                {error, _} ->
+                    io:fwrite("Wrong -pw argument value: ~p ~n", [NextArg]),
+                    exit(1);
+                {Integer, _} ->
+                    parse_parameters(Result#approx_params{points_window = Integer}, Tail)
             end;
         "-funcs" ->
             WithLinearFunc =
@@ -67,31 +68,21 @@ parse_parameters(Result, [_ | _]) ->
 parse_parameters(Result, []) ->
     Result.
 
-process_points(Points, Params) ->
+
+process_points(Params, Subscribers) ->
+    process_points({0, true}, Params, Subscribers).
+
+process_points({PreviousXValue, FirstRead}, Params, Subscribers) ->
     case io:fread("", "~f,~f") of
         {ok, [X, Y]} ->
-            NewPoints = lists:append(Points, [#point{x = X, y = Y}]),
-            case length(NewPoints) > 1 of
+            case (PreviousXValue < X) or FirstRead of
                 true ->
-                    [OldLastPoint | _] = lists:reverse(Points),
-                    case OldLastPoint#point.x < X of
-                        true ->
-                            XValues =
-                                approx_arg_gen:generate_arguments(OldLastPoint#point.x,
-                                                                  X,
-                                                                  Params#approx_params.step),
-                            run_approx_funcs(Params#approx_params.approx_funcs,
-                                             NewPoints,
-                                             XValues,
-                                             Params),
-                            io:fwrite("----------------------------------~n"),
-                            process_points(NewPoints, Params);
-                        _ ->
-                            io:fwrite("A next points X value must be greater than previous~n"),
-                            process_points(NewPoints, Params)
-                    end;
+                    send_point(#point{x = X, y = Y}, Subscribers),
+                    wait_and_process_replies(Subscribers),
+                    process_points({X, false}, Params, Subscribers);
                 _ ->
-                    process_points(NewPoints, Params)
+                    io:fwrite("A next points X value must be greater than previous~n"),
+                    process_points({PreviousXValue, false}, Params, Subscribers)
             end;
         eof ->
             ok;
@@ -99,55 +90,94 @@ process_points(Points, Params) ->
             exit(Reason)
     end.
 
-run_approx_funcs([], _, _, _) ->
-    ok;
-run_approx_funcs([FuncLiteral | Tail], ApproxPoints, XValues, Params) ->
-    Interval = {lists:nth(1, XValues), lists:last(XValues)},
-    case FuncLiteral of
-        l ->
-            case approx_funcs_methods:build_on_interval(ApproxPoints,
-                                                        fun approx_funcs_methods:calc_linear_least_squares/1,
-                                                        Interval,
-                                                        Params#approx_params.max_points)
-            of
-                not_possible ->
-                    io:fwrite("Linear approximation is not possible~n~n");
-                LinearFunc ->
-                    write_results(approx_funcs_methods:calc_arguments(LinearFunc, XValues),
-                                  "Linear approximation")
-            end;
-        e ->
-            case approx_funcs_methods:build_on_interval(ApproxPoints,
-                                                        fun approx_funcs_methods:calc_exp_least_squares/1,
-                                                        Interval,
-                                                        Params#approx_params.max_points)
-            of
-                not_possible ->
-                    io:fwrite("Exponential approximation is not possible~n~n");
-                ExpFunc ->
-                    write_results(approx_funcs_methods:calc_arguments(ExpFunc, XValues),
-                                  "Exponential approximation")
-            end;
-        p ->
-            case approx_funcs_methods:build_on_interval(ApproxPoints,
-                                                        fun approx_funcs_methods:calc_power_least_squares/1,
-                                                        Interval,
-                                                        Params#approx_params.max_points)
-            of
-                not_possible ->
-                    io:fwrite("Power approximation is not possible~n~n");
-                PowerFunc ->
-                    write_results(approx_funcs_methods:calc_arguments(PowerFunc, XValues),
-                                  "Power approximation")
-            end
-    end,
-    run_approx_funcs(Tail, ApproxPoints, XValues, Params).
+send_point(Point, Subscribers) ->
+    lists:foreach(
+        fun({_, Sub}) ->
+            Sub ! {send_point, self(), Point}
+        end,
+        Subscribers
+    ).
 
-write_results(Points, ApproxStr) ->
-    io:fwrite("~s: ~n", [ApproxStr]),
+wait_and_process_replies(Subscribers) ->
+    lists:foreach(
+        fun({Tag, _}) ->
+            receive
+                window_not_filled -> ok;
+                not_possible -> write_approx_not_possible_result(Tag);
+                {result, Points} -> write_approx_results(Points, Tag)
+            after 5000 -> exit(timeout)
+            end
+        end,
+        Subscribers
+    ).
+
+spawn_generators(Params) ->
+    lists:map(
+        fun(Atom) ->
+            case Atom of
+                l -> {l, spawn_link(
+                    fun() -> approx_generator(fun approx_funcs_methods:calc_linear_least_squares/1, [], Params) end)
+                };
+                e -> {e, spawn_link(
+                    fun() -> approx_generator(fun approx_funcs_methods:calc_exp_least_squares/1, [], Params) end)
+                };
+                p -> {p, spawn_link(
+                    fun() -> approx_generator(fun approx_funcs_methods:calc_power_least_squares/1, [], Params) end)
+                }
+            end
+        end,
+        Params#approx_params.approx_funcs
+    ).
+
+approx_generator(Generator, PointsWindow, Params) ->
+    receive
+        {send_point, SenderPid, Point} ->
+            NewWindow =
+                case length(PointsWindow) > Params#approx_params.points_window - 1 of
+                    true ->
+                        [_ | Tail] = PointsWindow,
+                        lists:append(Tail, [Point]);
+                    _ ->
+                        lists:append(PointsWindow, [Point])
+            end,
+            case length(NewWindow) >= Params#approx_params.points_window of
+                true ->
+                    [OldLastPoint | _] = lists:reverse(PointsWindow),
+                    XValues =
+                        approx_arg_gen:generate_arguments(OldLastPoint#point.x,
+                            Point#point.x,
+                            Params#approx_params.step),
+                    Interval = {lists:nth(1, XValues), lists:last(XValues)},
+                    case approx_funcs_methods:build_on_interval(NewWindow, Generator, Interval, Params#approx_params.points_window) of
+                        not_possible ->
+                            SenderPid ! not_possible;
+                        Func ->
+                            Result = approx_funcs_methods:calc_arguments(Func, XValues),
+                            SenderPid ! {result, Result}
+                    end;
+                _ ->
+                    SenderPid ! window_not_filled
+            end,
+            approx_generator(Generator, NewWindow, Params)
+    end.
+
+
+write_approx_results(Points, ApproxTag) ->
+    case ApproxTag of
+        l -> io:fwrite("~s: ~n", ["Linear approximation"]);
+        e -> io:fwrite("~s: ~n", ["Exponential approximation"]);
+        p -> io:fwrite("~s: ~n", ["Power approximation"])
+    end,
     io:fwrite("----------------------~n"),
     write_points(Points),
     io:fwrite("----------------------~n~n").
+
+write_approx_not_possible_result(ApproxTag) ->
+    case ApproxTag of
+        l -> io:fwrite("Linear approximation is not possible~n~n");
+        e -> io:fwrite("Exponential approximation is not possible~n~n");
+        p -> io:fwrite("Power approximation is not possible~n~n")
+    end.
 
 write_points([]) ->
     ok;
